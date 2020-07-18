feb34ff1108946b4dd25eb6899ace41db79bda6c
Gurdev Singh <gurdevs@Gurdevs-MacBook-Pro.local>
2020-07-18 20:10:54 +0530

    Simple HTTPS Server using Node.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + "/public"))
+
+http.listen(port, () => console.log(`Active on ${port} port`))
